951c6ca17de1de814143626b630456684fa4e3c0
Bhumi Patel <bhumipatel@TB5D6.WPA.Dal.Ca>
2019-03-05 17:30:47 -0400

    including object

--- app/src/main/java/com/example/focupokus/MainActivity.java
+++ app/src/main/java/com/example/focupokus/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.focupokus;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +13,55 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+import android.widget.ImageView;
+import java.util.logging.Logger;
+import java.util.Random;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    int shapes[] ={R.drawable.circle,R.drawable.cone,R.drawable.cylinder,R.drawable.hexagon,R.drawable.rectangle};
+    //int shapes[] ={R.drawable.circle,R.drawable.cone,R.drawable.cylinder,R.drawable.hexagon,R.drawable.ic_star,R.drawable.ic_star,R.drawable.ic_star,R.drawable.ic_star,R.drawable.ic_star,R.drawable.ic_star,R.drawable.ic_star};
+
+
+static int shapes[]={R.drawable.ic_circle,R.drawable.ic_pointed_star,R.drawable.ic_arrow_down_filled_triangle,R.drawable.ic_diamond,R.drawable.ic_hexagon,R.drawable.ic_circle,R.drawable.ic_circle,R.drawable.ic_circle,R.drawable.ic_circle};
+static int shapes1[]= new int [25];
+static Integer [] colour={Color.GREEN,Color.RED,Color.YELLOW,Color.BLUE,Color.CYAN, Color.MAGENTA,Color.LTGRAY,Color.BLACK};
+
+static HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+
+
+    public static Integer getRandomShape()
+    {
+        //int shapes[]={R.drawable.ic_circle,R.drawable.ic_pointed_star,R.drawable.ic_arrow_down_filled_triangle,R.drawable.ic_diamond,R.drawable.ic_hexagon,R.drawable.ic_circle};
+        int rnd = new Random().nextInt(shapes.length);
+        Log.d("shape", "shape" + rnd);
+        return shapes[rnd];
+    }
+
+    public void  getRandomShape1()
+    {
+        for(int i=0;i<16;i++)
+        {
+            int shape=getRandomShape();
+            shapes1[i]=shape;
+        }
+
+    }
+    public static void  getRandomShapewithcolour()
+    {
+        //int shapes[]={R.drawable.ic_circle,R.drawable.ic_pointed_star,R.drawable.ic_arrow_down_filled_triangle,R.drawable.ic_diamond,R.drawable.ic_hexagon,R.drawable.ic_circle};
+        for(int i=0;i<16;i++)
+        {
+            int rndshape = new Random().nextInt(shapes.length);
+            int randcolour = new Random().nextInt(colour.length);
+            hm.put(rndshape,randcolour);
+
+
+        }
+
+    }
+
     GridView grid;
     TextView tv;
     EditText et;
@@ -28,25 +76,29 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         grid = findViewById(R.id.hello);
+        grid.setNumColumns(4);
         tv=findViewById(R.id.textView);
         et=findViewById(R.id.et);
 
 
 
-        final String sc =et.getText().toString();
 
-        Adapter adapter = new Adapter(this, shapes);
+        final String sc =et.getText().toString();
+        getRandomShape1();
+        getRandomShapewithcolour();
+        final Adapter adapter = new Adapter(this,hm);
         grid.setAdapter(adapter);
+
+
 
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> s, View v, int position, long id) {
 
                 //if(getResources().getResourceEntryName(shapes[position]).equals(sc)) score++;
-                //Toast.makeText(getApplicationContext(),getResources().getResourceEntryName(shapes[position]),Toast.LENGTH_LONG).show();
-                tv.setText(getResources().getResourceEntryName(shapes[position]));
-
-
+                Toast.makeText(getApplicationContext(),getResources().getResourceEntryName(shapes1[position]),Toast.LENGTH_LONG).show();
+                long i = grid.getItemIdAtPosition(position);
+                tv.setText(getResources().getResourceEntryName(shapes1[position]));
 
             }
         });
