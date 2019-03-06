f92d1d7f60de469ccf81d4d43064a65c10d774f0
Ami <amitrivedi822@gmail.com>
2019-03-06 00:54:44 -0400

    Finished settings functionality

--- app/src/main/java/com/example/focupokus/start.java
+++ app/src/main/java/com/example/focupokus/start.java
@@ -1,31 +1,46 @@
 package com.example.focupokus;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class start extends AppCompatActivity {
-    Button round;
-
+    Button playButton, settingsButton,rulesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        round = findViewById(R.id.rou);
+        playButton = findViewById(R.id.playButton);
+        settingsButton = findViewById(R.id.settingsButton);
+        rulesButton = findViewById(R.id.rulesButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Dialog dialog = new Dialog(start.this);
+                dialog.setContentView(R.layout.activity_settings);
+
+                dialog.show();
+            }
+        });
     }
 
     public void play (View view) {
         Intent play = new Intent(this, MainActivity.class);
         startActivity(play);
     }
-
-    public void settings()
+    public void displayRules(View v)
     {
-
+        Dialog dialog = new Dialog(start.this);
+        dialog.setContentView(R.layout.activity_settings);
+        dialog.show();
     }
 }
