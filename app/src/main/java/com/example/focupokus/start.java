93e73b38408d4561338129e83aac6722b8b38a4a
Bhumi Patel <bhumipatel@Bhumis-MacBook-Pro.local>
2019-03-28 01:05:15 -0300

    add functionality onMusic click

--- app/src/main/java/com/example/focupokus/start.java
+++ app/src/main/java/com/example/focupokus/start.java
@@ -97,6 +97,32 @@ public class start extends AppCompatActivity {
 
                     }
                 });
+                vibrateSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        meditor.putBoolean("soundSwitchValue",soundSwitch.isChecked());
+                        soundSwitch.setChecked(soundSwitch.isChecked());
+                        meditor.commit();
+                        boolean isMusic= mPreference.getBoolean("soundSwitchValue",true);
+                        Log.d("boolean", "boolean"+isMusic);
+                        //((start)getActivity()).sound=soundSwitch.isChecked();
+
+                    }
+                });
+                musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        meditor.putBoolean("soundSwitchValue",soundSwitch.isChecked());
+                        soundSwitch.setChecked(soundSwitch.isChecked());
+                        meditor.commit();
+                        boolean isMusic= mPreference.getBoolean("soundSwitchValue",true);
+                        Log.d("boolean", "boolean"+isMusic);
+                        //((start)getActivity()).sound=soundSwitch.isChecked();
+
+                    }
+                });
 
 
 
